928ee2605756121e7ec4fa281f4db9c6bfecb6cb
jgordini <jgordini@uab.edu>
2022-01-03 15:42:59 -0600

    builder update

--- scripts/role.builder.js
+++ scripts/role.builder.js
@@ -12,16 +12,19 @@ var roleBuilder = {
         }
         if (creep.memory.building){
             var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-
-
+            if(targets.length) {
+                if (creep.build(targets[0])===ERR_NOT_IN_RANGE){
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#fff'}});
+                }
         }
     }
-    else if(Game.spawns['Spawn1'].energy < Game.spawns['Spawn1'].energyCapacity) {
-    if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(Game.spawns['Spawn1']);
+    else {
+        var sources = creep.room.find(FIND_SOURCES);
+        if(creep.harvest(sources[0])===ERR_NOT_IN_RANGE){
+            creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+        }
     }
-}
-}
 };
 
 module.exports = roleBuilder;
