14ec71d9e6e7de5466c6fbc3f0b8a795dfe99e8e
jgordini <jgordini@uab.edu>
2022-01-31 15:25:13 -0600

    harvest and upgrade

--- scripts/role.harvester.js
+++ scripts/role.harvester.js
@@ -11,9 +11,7 @@ var roleHarvester = {
         else {
             var targets = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION ||
-                            structure.structureType == STRUCTURE_SPAWN ||
-                            structure.structureType == STRUCTURE_TOWER) &&
+                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
                         structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                 }
             });
