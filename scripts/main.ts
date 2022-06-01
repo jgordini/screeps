5713d07632377766c80f455580c992f721f7665c
jgordini <jgordini@uab.edu>
2022-06-01 09:49:12 -0500

    var to const

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,28 +1,28 @@
-var Harvester = require('role.harvester');
-var Upgrader = require('role.upgrader');
-interface CreepMemory { [name: string]: any };
+const Harvester = require('role.harvester');
+const Upgrader = require('role.upgrader');
+interface CreepMemory { [name: string]: any }
 
 module.exports.loop = function () {
 
-    for(var name in Memory.creeps) {
+    for(const name in Memory.creeps) {
         if(!Game.creeps[name]) {
             delete Memory.creeps[name];
             console.log('Clearing non-existing creep memory:', name);
         }
     }
 
-    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    const harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     console.log('Harvesters: ' + harvesters.length);
 
     if(harvesters.length < 2) {
-        var newName = 'Harvester' + Game.time;
+        const newName = 'Harvester' + Game.time;
         console.log('Spawning new harvester: ' + newName);
         Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName,
             {memory: {role: 'harvester'}});
     }
 
     if(Game.spawns['Spawn1'].spawning) {
-        var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
+        const spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
         Game.spawns['Spawn1'].room.visual.text(
             '🛠️' + spawningCreep.memory.role,
             Game.spawns['Spawn1'].pos.x + 1,
@@ -30,8 +30,8 @@ module.exports.loop = function () {
             {align: 'left', opacity: 0.8});
     }
 
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
+    for(const name in Game.creeps) {
+        const creep = Game.creeps[name];
         if(creep.memory.role == 'harvester') {
             Harvester.run(creep);
         }
